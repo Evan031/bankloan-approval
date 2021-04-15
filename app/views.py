119e74f09af0188f8c924d74dcefb3ef2931324a
Evan Taylor <evan.taylor@uni4online.co.za>
2021-04-15 22:21:04 +0200

    Added templating

--- app/views.py
+++ app/views.py
@@ -3,4 +3,7 @@ from django.contrib import messages
 
 
 def model_form(request):
-    return render(request, 'myform/model-form.html')
+    return render(request, 'app/model_form.html')
+
+# def template_example_1(request):
+#     return render(request,"child_page_1.html")
